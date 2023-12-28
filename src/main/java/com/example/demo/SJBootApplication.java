5d99aa2f936e555450fe42f51a44a6e4fb287422
Park Sung Jun <junstin119@gmail.com>
2023-12-29 00:08:47 +0900

    프론트 컨트롤러
    - 기존 서플릿을 프론트 컨트롤로 전환
    - boot로 만든 Hello의 역할을 해당 서블릿코드로 구현

--- src/main/java/com/example/demo/SJBootApplication.java
+++ src/main/java/com/example/demo/SJBootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -24,16 +25,23 @@ public class SJBootApplication {
 		WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
 			@Override
 			public void onStartup(ServletContext servletContext) throws ServletException {
-				servletContext.addServlet("hello", new HttpServlet() {
+				servletContext.addServlet("frontcontroller", new HttpServlet() {
 					@Override
 					protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-						String name = req.getParameter("name");
+						if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+							String name = req.getParameter("name");
 
-						resp.setStatus(HttpStatus.OK.value());
-						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println("Hello " + name);
+							resp.setStatus(HttpStatus.OK.value());
+							resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+							resp.getWriter().println("Hello " + name);
+						}else if(req.getRequestURI().equals("/user")) {
+
+						}else {
+							resp.setStatus(HttpStatus.NOT_FOUND.value());
+						}
+
 					}
-				}).addMapping("/hello");
+				}).addMapping("/*"); // 프론트컨트롤러 - /아래로 들어오는 모든 요청을 처리함
 			}
 		});
 		webServer.start();
