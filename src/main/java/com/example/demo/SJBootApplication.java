5b75a1c75e9fa85419b636223eaa9452754a44f0
Park Sung Jun <junstin119@gmail.com>
2023-12-31 21:38:03 +0900

    DispactcherServlet 전환

--- src/main/java/com/example/demo/SJBootApplication.java
+++ src/main/java/com/example/demo/SJBootApplication.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -22,37 +24,17 @@ import java.io.IOException;
 public class SJBootApplication {
 
 	public static void main(String[] args) {
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 		applicationContext.registerBean(HelloController.class); // bin등록방법을 클래스 정보만 넘겨줌.
 		applicationContext.registerBean(SimpleHellowService.class); // 생성자를 호출할때 컨테이너의 등록, 해당 클래스를 찾아 생성자에 전달
 		applicationContext.refresh();
 
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
-
-			@Override
-			public void onStartup(ServletContext servletContext) throws ServletException {
-				servletContext.addServlet("hello", new HttpServlet() {
-					@Override
-					protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-						// 프론트 컨트롤러를 통해 인증, 보안, 다국어, 공통기능을 처리
-						if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-							String name = req.getParameter("name");
-
-							HelloController helloController = applicationContext.getBean(HelloController.class);// 클래스타입을 적어서 해당빈이 있으면 반환
-							String ret = helloController.hello(name);
-
-							resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-							resp.getWriter().println(ret);
-						}
-						else {
-							resp.setStatus(HttpStatus.NOT_FOUND.value());
-						}
-
-					}
-				}).addMapping("/*"); // 프론트컨트롤러 - /아래로 들어오는 모든 요청을 처리함
-			}
-		});
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("dispatcherServlet",
+                        new DispatcherServlet(applicationContext)
+                    ).addMapping("/*"); // 프론트컨트롤러 - /아래로 들어오는 모든 요청을 처리함
+        });
 		webServer.start();
 	}
 
