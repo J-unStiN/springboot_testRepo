74d298832db38932c7f07dda095395deb40afab7
Park Sung Jun <junstin119@gmail.com>
2024-01-01 21:22:42 +0900

    동적 Configration 등록
    - importSelector 를 통해 필요한 Config들의 경로를 문자열로 반환해 등록

--- src/main/java/com/example/config/EnableMyAutoConfiguration.java
+++ src/main/java/com/example/config/EnableMyAutoConfiguration.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Import({TomcatWebServerConfig.class, DispatcherServletConfig.class})
+@Import(MyAutoConfigImportSelector.class)
 public @interface EnableMyAutoConfiguration {
 
 }
