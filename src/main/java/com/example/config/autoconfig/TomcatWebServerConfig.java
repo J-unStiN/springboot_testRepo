896bcd61e375c6447142fedbf029909fbe5f1c64
Park Sung Jun <junstin119@gmail.com>
2024-01-06 00:01:36 +0900

    PropertySourcesPlaceholderConfigurer
    - application.properties에 적힌 contextPath를 필드에서 읽음
    - PropertySourcesPlaceholderConfigure을 통해 플레이스 홀더에 있는 값을 변환

--- src/main/java/com/example/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/example/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package com.example.config.autoconfig;
 
 import com.example.config.ConditionalMyOnClass;
 import com.example.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -13,11 +14,15 @@ import org.springframework.util.ClassUtils;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
+    @Value("${contextPath}")
+    String contextPath;
+
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
     public ServletWebServerFactory serverFactory(Environment env) {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(env.getProperty("contextPath"));
+        //factory.setContextPath(env.getProperty(this.contextPath));
+        factory.setContextPath(this.contextPath);
         return factory;
     }
 
