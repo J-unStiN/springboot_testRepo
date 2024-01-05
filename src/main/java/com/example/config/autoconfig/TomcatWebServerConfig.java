2f689ec3737d62f55286844f459f9b2ef413fec1
Park Sung Jun <junstin119@gmail.com>
2024-01-06 00:47:55 +0900

    프로퍼티 빈 후처리
    - 프로퍼티바인딩을 할 때 앞에 prefix(server)를 붙여서 그 뒤에 나오는 이름이 클래스의 프로퍼티 이름과 같은지 확인.
    
    - 자동구성의 오버라이딩해서 세밀한 설정이 가능하게 함.

--- src/main/java/com/example/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/example/config/autoconfig/TomcatWebServerConfig.java
@@ -1,6 +1,7 @@
 package com.example.config.autoconfig;
 
 import com.example.config.ConditionalMyOnClass;
+import com.example.config.EnableMyConfigurationProperties;
 import com.example.config.MyAutoConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -13,6 +14,7 @@ import org.springframework.util.ClassUtils;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
+@EnableMyConfigurationProperties(ServerProperties.class)
 public class TomcatWebServerConfig {
 
     @Bean("tomcatWebServerFactory")
