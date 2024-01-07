6ae47ba9b6ba6b767d8229a13cd1ae7ad05a3058
Park Sung Jun <junstin119@gmail.com>
2024-01-07 12:58:44 +0900

    jdbcTemplate, 트랜잭션 매니저 구성
    - db 테스트를 위한 테스트용 어노테이션 생성
    - jbdc 테스트
    - rollback(false) 하면 커밋하고 롤백을 안시킴

--- src/main/java/com/example/config/autoconfig/DataSourceConfig.java
+++ src/main/java/com/example/config/autoconfig/DataSourceConfig.java
@@ -5,8 +5,13 @@ import com.example.config.EnableMyConfigurationProperties;
 import com.example.config.MyAutoConfiguration;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.sql.Driver;
@@ -15,6 +20,7 @@ import java.sql.Driver;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
 @EnableMyConfigurationProperties(MyDatasourceProperties.class)
+@EnableTransactionManagement
 public class DataSourceConfig {
 
     @Bean
@@ -41,6 +47,19 @@ public class DataSourceConfig {
         dataSource.setPassword(properties.getPassword());
 
         return dataSource;
+    }
+    @Bean
+    @ConditionalOnSingleCandidate(DataSource.class) // 빈 구성정보에 해당 타입의 빈이 하나만 등록되어있다면 사용함
+    @ConditionalOnMissingBean
+    JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    @ConditionalOnSingleCandidate(DataSource.class) // 빈 구성정보에 해당 타입의 빈이 하나만 등록되어있다면 사용함
+    @ConditionalOnMissingBean
+    JdbcTransactionManager jdbcTransactionManager(DataSource dataSource) {
+        return new JdbcTransactionManager(dataSource);
     }
 
 
--- src/test/java/com/example/demo/DataSourceTest.java
+++ src/test/java/com/example/demo/DataSourceTest.java
@@ -7,14 +7,13 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-@ExtendWith(SpringExtension.class) // 스프링 컨텍스트를 이용해 스프링 컨테이너 테스트 가능
-@ContextConfiguration(classes = SJBootApplication.class)
-@TestPropertySource("classpath:/application.properties")
+@HellobootTest
 public class DataSourceTest {
     @Autowired
     DataSource dataSource;
