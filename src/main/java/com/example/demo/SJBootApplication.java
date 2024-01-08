86721dd231990106b63f79ce2fa43439cf3ec30e
Park Sung Jun <junstin119@gmail.com>
2024-01-09 01:57:33 +0900

    스프링 부트의 자동 구성과 테스트로 전환
    - 기존 코드를 스프링 부트로 만들어진 코드로 전환
    - 스프링 부트 테스트 추가

--- src/main/java/com/example/demo/SJBootApplication.java
+++ src/main/java/com/example/demo/SJBootApplication.java
@@ -1,26 +1,19 @@
 package com.example.demo;
 
 
-import com.example.config.MySpringBootApplication;
+
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.annotation.PostConstruct;
 
-@MySpringBootApplication
+@SpringBootApplication
 public class SJBootApplication {
-
-//	@Bean
-//	ApplicationRunner applicationRunner(Environment env) {
-//		return args -> {
-//			String name = env.getProperty("my.name");
-//			System.out.println("my.name = " + name);
-//		};
-//	}
 
 	private final JdbcTemplate jdbcTemplate;
 
--- src/test/java/com/example/demo/HelloServiceCountTest.java
+++ src/test/java/com/example/demo/HelloServiceCountTest.java
@@ -1,16 +1,15 @@
 package com.example.demo;
 
-import com.example.demo.HelloRepository;
-import com.example.demo.HellobootTest;
-import com.example.demo.HellowService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.stream.IntStream;
 
-@HellobootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Transactional
 public class HelloServiceCountTest {
     @Autowired
     HellowService hellowService;
